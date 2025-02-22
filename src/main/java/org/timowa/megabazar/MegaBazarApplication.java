f4c9ea93e65ebda44134f16416643e9bfa611045
Timowa <sd3059272@gmail.com>
2025-02-22 17:23:39 +0500

    Init

---
+++ src/main/java/org/timowa/megabazar/MegaBazarApplication.java
@@ -0,0 +1,13 @@
+package org.timowa.megabazar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegaBazarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MegaBazarApplication.class, args);
+    }
+
+}
