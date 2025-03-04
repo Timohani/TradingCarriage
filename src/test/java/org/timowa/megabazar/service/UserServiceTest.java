fc8bc60a93d48a9ac4a44b315447eca1be3b48ba
Timowa <sd3059272@gmail.com>
2025-03-04 20:06:48 +0500

    test: Изменить тест регистрации и добавить тест получения по id

--- src/test/java/org/timowa/megabazar/service/UserServiceTest.java
+++ src/test/java/org/timowa/megabazar/service/UserServiceTest.java
@@ -5,8 +5,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import org.timowa.megabazar.dto.UserInfoDto;
 import org.timowa.megabazar.dto.UserReadDto;
 import org.timowa.megabazar.dto.UserRegistrationDto;
+import org.timowa.megabazar.exception.UserAlreadyExistsException;
+import org.timowa.megabazar.exception.UserNotFoundException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,19 +22,38 @@ class UserServiceTest {
     @Test
     void registrationTest() {
         UserRegistrationDto invalidUser =
-                new UserRegistrationDto("oleg",
+                new UserRegistrationDto(
+                        "oleg",
                         "oleg", // Invalid Email
                         ""); // Invalid password
         assertThrows(ConstraintViolationException.class,
                 () -> service.registration(invalidUser));
 
-        UserRegistrationDto user =
-                new UserRegistrationDto("oleg",
+        UserRegistrationDto validUser =
+                new UserRegistrationDto(
+                        "oleg",
                         "oleg@gmail.com",
                         "1234");
-        UserReadDto resultUser = service.registration(user);
+        UserReadDto resultUser = service.registration(validUser);
 
         assertNotNull(resultUser);
-        assertEquals("oleg", resultUser.getUsername());
+        assertEquals(validUser.getUsername(), resultUser.getUsername());
+
+        assertThrows(UserAlreadyExistsException.class,
+                () -> service.registration(validUser));
+    }
+
+    @Test
+    void getTest() {
+        assertThrows(UserNotFoundException.class,
+                () -> service.getUser(993L));
+
+        UserRegistrationDto userToSave = new UserRegistrationDto(
+                "pavel",
+                "pavel229@mail.ru",
+                "22882");
+        UserReadDto user = service.registration(userToSave);
+        UserInfoDto findUser = service.getUser(user.getId());
+        assertEquals(user.getUsername(), findUser.getUsername());
     }
 }
