82557680712c08d668640f9bfb6366ddc8710206
Timowa <sd3059272@gmail.com>
2025-04-16 18:52:28 +0500

    feat: Замена текста на enum в качестве роли для User

---
+++ src/main/java/org/timowa/megabazar/database/entity/Role.java
@@ -0,0 +1,5 @@
+package org.timowa.megabazar.database.entity;
+
+public enum Role {
+    USER, SELLER, ADMIN
+}
--- src/main/java/org/timowa/megabazar/database/entity/User.java
+++ src/main/java/org/timowa/megabazar/database/entity/User.java
@@ -32,7 +32,8 @@ public class User {
     private String email;
 
     @Column(nullable = false)
-    private String role = "USER";
+    @Enumerated
+    private Role role = Role.USER;
 
     @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
