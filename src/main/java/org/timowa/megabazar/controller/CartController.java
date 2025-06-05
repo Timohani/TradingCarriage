0c486a1e03d3e9f4a54598a088c156f1274bc76c
timohani <sd3059272@gmail.com>
2025-06-05 13:05:07 +0500

    feat: Добавить контроллер для просмотра и изменения корзины

---
+++ src/main/java/org/timowa/megabazar/controller/CartController.java
@@ -0,0 +1,55 @@
+package org.timowa.megabazar.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.timowa.megabazar.database.entity.Cart;
+import org.timowa.megabazar.database.entity.CartItem;
+import org.timowa.megabazar.database.repository.CartItemRepository;
+import org.timowa.megabazar.dto.cartItem.CartItemReadDto;
+import org.timowa.megabazar.exception.CartLimitExceededException;
+import org.timowa.megabazar.exception.ProductNotAvailableException;
+import org.timowa.megabazar.mapper.cartItem.CartItemReadMapper;
+import org.timowa.megabazar.service.CartService;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/cart")
+@RequiredArgsConstructor
+public class CartController {
+
+    private final CartItemRepository cartItemRepository;
+    private final CartService cartService;
+
+    private final CartItemReadMapper cartItemReadMapper;
+
+    @GetMapping
+    public PagedModel<CartItemReadDto> getCart(@ParameterObject Pageable pageable) {
+        Cart cart = cartService.getCurrentUserCart();
+        Page<CartItemReadDto> cartItems = cartItemRepository.findAllByCartId(cart.getId(), pageable).map(cartItemReadMapper::map);
+        return new PagedModel<>(cartItems);
+    }
+
+    @GetMapping("/{id}")
+    public CartItemReadDto getOneById(@PathVariable Long id) {
+        Optional<CartItem> cartItemOptional = cartItemRepository.findById(id);
+        return cartItemOptional.map(cartItemReadMapper::map).orElseThrow(() ->
+                new ResponseStatusException(HttpStatus.NOT_FOUND, "Entity with id `%s` not found".formatted(id)));
+    }
+
+    @PostMapping
+    public CartItemReadDto addItemToCart(@RequestParam Long productId) throws CartLimitExceededException, ProductNotAvailableException {
+        return cartService.addItemToCart(productId);
+    }
+
+    @DeleteMapping("/{productId}")
+    public CartItemReadDto removeItemFromCart(@PathVariable Long productId) {
+        return cartService.removeItemFromCart(productId);
+    }
+}
--- src/main/java/org/timowa/megabazar/database/repository/CartItemRepository.java
+++ src/main/java/org/timowa/megabazar/database/repository/CartItemRepository.java
@@ -1,5 +1,7 @@
 package org.timowa.megabazar.database.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.timowa.megabazar.database.entity.Cart;
 import org.timowa.megabazar.database.entity.CartItem;
@@ -10,4 +12,6 @@ import java.util.Optional;
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 
     Optional<CartItem> findByCartAndProduct(Cart cart, Product product);
+
+    Page<CartItem> findAllByCartId(Long cartId, Pageable pageable);
 }
