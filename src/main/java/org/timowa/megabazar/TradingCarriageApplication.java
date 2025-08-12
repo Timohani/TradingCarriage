8a9696806b4c01357bb0daadcd36637625c76a86
timohani <sd3059272@gmail.com>
2025-08-12 20:33:01 +0500

    refactor: Переименовать файл запуска

--- src/main/java/org/timowa/megabazar/TradingCarriageApplication.java
+++ src/main/java/org/timowa/megabazar/TradingCarriageApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MegaBazarApplication {
+public class TradingCarriageApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MegaBazarApplication.class, args);
+        SpringApplication.run(TradingCarriageApplication.class, args);
     }
 
 }
